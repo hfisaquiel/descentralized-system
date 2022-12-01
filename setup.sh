b7a4b11492e24f7a1e73d3cab2bd65ef801b1f15
Henry Franklin <henrymais@gmail.com>
2022-11-30 23:07:35 -0300

    feat: add functions for iterate over folder projects structure

--- setup.sh
+++ setup.sh
@@ -3,7 +3,41 @@ set -e
 
 export INTERNAL_NETWORK_NAME=descentralized-system-network
 
-echo "Creating main network ${INTERNAL_NETWORK_NAME}"
+SCRIPT_DIR=$PWD
+
+runDockerComposeRebuild() {
+    docker-compose up -d --build --force
+}
+
+runComposeOnScopeProjectList() {
+    local FOLDER_PATH_KEEP=$PWD
+
+    cd "$1"
+
+    local CURRENT_FOLDER_PATH=$PWD
+
+    find "." -mindepth 1 -maxdepth 1 -type d | while read dir_name; do
+        local path_compose="${dir_name}/docker-compose.yml"
+        if [ -e "$path_compose" ]; then
+            cd "$dir_name"
+
+            echo "runnig $path_compose"
+            runDockerComposeRebuild
+
+            cd "$CURRENT_FOLDER_PATH"
+        fi
+    done
+
+    cd "$FOLDER_PATH_KEEP"
+}
+
+echo ''
+echo '\e[1;31;1m+-----------------------+\e[0m'
+echo '\e[1;31;1m| Descentralized System |\e[0m'
+echo '\e[1;31;1m+-----------------------+\e[0m'
+
+echo ''
+echo "\e[1;34;1mCreating main network ${INTERNAL_NETWORK_NAME}\e[0m"
 docker network create \
     --attachable \
     --internal \
@@ -12,8 +46,10 @@ docker network create \
     --ip-range=172.32.10.0/24 \
     ${INTERNAL_NETWORK_NAME} || true
 
-echo 'Bringing up Proxy'
+echo ''
+echo '\e[34;1mBringing up Proxy\e[0m'
 cd -P ./proxy || {
     echo 'No Proxy. aborting!'
     exit 1
-} && docker-compose up -d --build
+} && runDockerComposeRebuild
+cd "$SCRIPT_DIR"
